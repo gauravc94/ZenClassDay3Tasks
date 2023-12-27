d49db27d621f88737321eb0bdf68abae19bca58e
Gaurav Chaubey <35659685+gauravc94@users.noreply.github.com>
2023-12-27 18:39:09 +0530

    Create 1d.PrimeNumbers.js
    A .js file to return all the prime numbers in an Array using an Anonymous Function and an IIFE.

---
+++ Day4Tasks/1d.PrimeNumbers.js
@@ -0,0 +1,43 @@
+// Return all the prime numbers in an array
+
+// We take an example array of numbers from 1 to 15
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+// Using an anonymous function
+const primeNumbersAnon = function(arr){
+    arr.forEach(number => {
+        let isPrime = true;
+
+        for (let i = 2; i < number; i++) {
+            if (number % i === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime && number > 1) {
+            console.log(number);
+        }
+    });
+}
+
+primeNumbersAnon(numbers);
+
+
+// Using an IIFE
+(function (){
+    numbers.forEach(number => {
+        let isPrime = true;
+
+        for (let i = 2; i < number; i++) {
+            if (number % i === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime && number > 1) {
+            console.log(number);
+        }
+    });
+})();
